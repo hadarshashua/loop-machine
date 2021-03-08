257dca9e78430239abbeeeee9e0a9f2212bf52ca
hadarshashua <58817024+hadarshashua@users.noreply.github.com>
2021-03-09 00:20:36 +0200

    handle record + refactor code

--- out/artifacts/moveo_interview_war_exploded/scripts/page.js
+++ out/artifacts/moveo_interview_war_exploded/scripts/page.js
@@ -1,35 +1,35 @@
+const AUDIO_LENGTH_MILLISECONDS = 8000;
 
-var squaresArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-var loopsNamesArray = new Array("resources/loops/120_future_funk_beats_25.mp3", "resources/loops/120_stutter_breakbeats_16.mp3", "resources/loops/FUD_120_StompySlosh.mp3", "resources/loops/GrooveB_120bpm_Tanggu.mp3", "resources/loops/MazePolitics_120_Perc.mp3", "resources/loops/SilentStar_120_Em_OrganSynth.mp3", "resources/loops/Bass%20Warwick%20heavy%20funk%20groove%20on%20E%20120%20BPM.mp3", "resources/loops/electric%20guitar%20coutry%20slide%20120bpm%20-%20B.mp3", "resources/loops/PAS3GROOVE1.03B.mp3");
-var audiosArray = [];
-var mediaRecorder;
-var audioChunks = [];
-var audioUrl;
-var shouldPlay = false;
+let squaresArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+const loopsNamesArray = new Array("resources/loops/120_future_funk_beats_25.mp3", "resources/loops/120_stutter_breakbeats_16.mp3", "resources/loops/FUD_120_StompySlosh.mp3", "resources/loops/GrooveB_120bpm_Tanggu.mp3", "resources/loops/MazePolitics_120_Perc.mp3", "resources/loops/SilentStar_120_Em_OrganSynth.mp3", "resources/loops/Bass%20Warwick%20heavy%20funk%20groove%20on%20E%20120%20BPM.mp3", "resources/loops/electric%20guitar%20coutry%20slide%20120bpm%20-%20B.mp3", "resources/loops/PAS3GROOVE1.03B.mp3");
+const audiosArray = [];
+let mediaRecorder;
+let audioChunks = [];
+let audioUrl;
+let audioRecord;
+let shouldPlay = false;
 
 $(function() {
     initializeAudioArray();
     loadFromStorage();
-    var button = document.getElementById("play-session-button");
-    button.classList.add("display-none");
+    $("#play-session-button").addClass("display-none");
+    $("#audio").attr("crossOrigin", "anonymous");
 });
 
 function initializeAudioArray() {
-    for(i=0; i<loopsNamesArray.length; i++)
-    {
-        var audio = new Audio(loopsNamesArray[i]);
-        audiosArray.push(audio);
+    for(i=0; i<loopsNamesArray.length; i++) {
+        audiosArray.push(new Audio(loopsNamesArray[i]));
     }
 }
 
 function loadFromStorage() {
-    document.getElementById("load").disabled = (localStorage.getItem("buttons")) ? false : true;
+    $("#load").prop("disabled",(localStorage.getItem("buttons")) ? false : true);
 }
 
 function changeSquareState(squareId) {
-    var element = document.getElementById(squareId);
-    var currState = element.childNodes[0].nodeValue;
-    var numOfSquare = squareId.replace( /^\D+/g, ''); // replace all leading non-digits with nothing
+    const element = document.getElementById(squareId);
+    const currState = element.childNodes[0].nodeValue;
+    const numOfSquare = squareId.replace( /^\D+/g, ''); // replace all leading non-digits with nothing
 
     if(currState === "On") {
         element.childNodes[0].nodeValue = "Off";
@@ -37,8 +37,7 @@ function changeSquareState(squareId) {
         squaresArray[numOfSquare - 1] = 0;
         audiosArray[numOfSquare - 1].pause();
         audiosArray[numOfSquare - 1].currentTime = 0; //get back to start position of audio
-    }
-    else {
+    } else {
         element.childNodes[0].nodeValue = "On";
         element.classList.add("clicked_square");
         squaresArray[numOfSquare - 1] = 1;
@@ -52,8 +51,7 @@ function playLoops() {
     setInterval(() => {
         if(shouldPlay)
             startLoops();
-
-    }, 8000);
+    }, AUDIO_LENGTH_MILLISECONDS);
 }
 
 function startLoops() {
@@ -77,15 +75,14 @@ function stopLoops() {
 }
 
 function handleRecording() {
-    document.getElementById("switch").checked ? startRecording() : stopRecording();
+    $("#switch").is(':checked') ? startRecording() : stopRecording();
 }
 
 function startRecording() {
     audioChunks = [];
     navigator.mediaDevices.getUserMedia({ audio: true })
         .then(stream => {
-            var mRecorder = new MediaRecorder(stream);
-            mediaRecorder = mRecorder;
+            mediaRecorder = new MediaRecorder(stream);
             mediaRecorder.start();
 
             mediaRecorder.addEventListener("dataavailable", event => {
@@ -98,6 +95,8 @@ function stopRecording() {
     mediaRecorder.addEventListener("stop", () => {
         const audioBlob = new Blob(audioChunks);
         audioUrl = URL.createObjectURL(audioBlob);
+        $("#audio").attr('src', audioUrl);
+        // audioRecord = new Audio(audioUrl);
     });
 
     mediaRecorder.stop();
@@ -105,44 +104,59 @@ function stopRecording() {
 }
 
 function showPlaySessionButton() {
-    var button = document.getElementById("play-session-button");
-    button.classList.remove("display-none");
-    button.classList.add("display-flex");
+    const button = $("#play-session-button");
+    button.removeClass("display-none");
+    button.addClass("display-flex");
 }
 
 function playSession() {
-    const audio = new Audio(audioUrl);
-    audio.play();
+    //const audio = new Audio(document.getElementById("audio"));
+    document.getElementById("audio").play();
+    //const audio = new Audio($("#audio"));
+    //audio.play().then(()=>{}).catch(err => console.log(err));
+    //audioRecord = new Audio(audioUrl);
+    //audio.play();
 }
 
 function saveUserActions() {
     localStorage.setItem("buttons", JSON.stringify(squaresArray));
-    // localStorage.setItem("audioUrl", JSON.stringify(audioUrl));
-    // localStorage.setItem("audioChunks", JSON.stringify(audioChunks));
-    // localStorage.setItem("mediaRecorder", JSON.stringify(mediaRecorder));
-    document.getElementById("load").disabled = false;
+    //localStorage.setItem("audioRecord", audioRecord);
+    localStorage.setItem("audioUrl", audioUrl);
+    //localStorage.setItem("audioChunks", JSON.stringify(audioChunks));
+    //localStorage.setItem("mediaRecorder", JSON.stringify(mediaRecorder));
+    $("#load").attr("disabled", false);
 }
 
 function RestoreUserActions() {
     squaresArray = JSON.parse(localStorage.getItem("buttons"));
-    // var url = JSON.parse(localStorage.getItem("audioUrl"));
-    // if (url)
-    // {
-    //     // audioUrl = url;
-    //     audioChunks = JSON.parse(localStorage.getItem("audioChunks"));
-    //     const audioBlob = new Blob(audioChunks);
-    //     audioUrl = URL.createObjectURL(audioBlob);
-    //     // mediaRecorder = JSON.parse(localStorage.getItem("mediaRecorder"));
-    //     showPlaySessionButton();
-    //
-    // }
+    let url = localStorage.getItem("audioUrl");
+    //let audioChunksFromStorage = JSON.parse(localStorage.getItem("audioChunks"));
+    if (url)
+    {
+        audioUrl = url;
+        //audioChunks = JSON.parse(localStorage.getItem("audioChunks"));
+        //audioChunks = audioChunksFromStorage;
+        //const audioBlob = new Blob(audioChunks);
+        //audioUrl = URL.createObjectURL(audioBlob);
+        //audioRecord = new Audio(audioUrl);
+        $("#audio").attr('src', audioUrl);
+        //audioRecord = localStorage.getItem("audioRecord");
+        // mediaRecorder = JSON.parse(localStorage.getItem("mediaRecorder"));
+        showPlaySessionButton();
+    }
 
     for(i=0; i<squaresArray.length; i++) {
+        let squareId = "square" + (i+1).toString();
+        let element = document.getElementById(squareId);
+
         if(squaresArray[i] === 1) {
-            var squareId = "square" + (i+1).toString();
-            var element = document.getElementById(squareId);
             element.childNodes[0].nodeValue = "On";
             element.classList.add("clicked_square");
+        }
+        else
+        {
+            element.childNodes[0].nodeValue = "Off";
+            element.classList.remove("clicked_square");
         }
     }
 }
